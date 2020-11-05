7f46d1e705129bcd4725fcdd760bf4e6974f843b
sagivbgu <sagivsc@post.bgu.ac.il>
2020-11-05 10:39:56 +0200

    Added: Read image and transformation file

--- translate.py
+++ translate.py
@@ -2,19 +2,39 @@ from sys import argv
 import transform
 import interpolate
 
-def read_image_file(path):
-    pass
 
-def read_tran_file(path):
-    pass
+def load_image_file(path):
+    try:
+        with open(path, 'rb') as img_file:
+            return img_file.read()
+    except Exception as e:
+        raise Exception("Error reading image file {0}".format(path)) from e
+
+
+def load_trans_file(path):
+    try:
+        with open(path, 'r') as img_file:
+            return (tuple(line.split()) for line in img_file.readlines())
+    except Exception as e:
+        raise Exception("Error reading transformation file {0}".format(path)) from e
+
+
+def translate_image(image_path, transformations_path, quality):
+    img = load_image_file(image_path)
+    transformations = load_trans_file(transformations_path)
+
+    # m = transformation_to_matrices(transformations)
+    # new_img = interpolate(img, quality)  # quality: nearest neighbor, bilinear interpolation, or cubic
+    # save_image(new_img)
 
 
 def main():
-    # translate image tran quality
     if len(argv) != 4:
         print("Usage {0} image_file tran_file quality".format(argv[0]))
         return
 
+    translate_image(argv[1], argv[2], argv[3])
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
