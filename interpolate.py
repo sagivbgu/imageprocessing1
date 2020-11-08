d95243bf4415a1894da5b67db199ac90be4e73bd
sagivbgu <sagivsc@post.bgu.ac.il>
2020-11-08 18:45:35 +0200

    Added cubic interpolation draft

--- interpolate.py
+++ interpolate.py
@@ -1,3 +1,4 @@
+import numpy as np
 import math
 from transform import calc_coordinates
 from transform import does_exceed
@@ -52,19 +53,66 @@ def interpolation_bilinear(new_img, original_img, inverse_transformation):
             intensity_bottom = (1 - width) * original_img[bottom_i][left_j] + width * original_img[bottom_i][right_j]
             new_img[new_i][new_j] = (1 - height) * intensity_bottom + height * intensity_top
 
-# def interpolation_cubic(new_img, original_img, inverse_transformation):
-#     new_rows, new_cols = new_img.shape
-#     old_rows, old_cols = original_img.shape
-#     for new_i in range(new_rows):
-#         for new_j in range(new_cols):
-#             old_i, old_j = calc_coordinates(inverse_transformation, new_i, new_j)
-#             top_i = 0 if round(old_i) == 0 else round(old_i) - 1
-#             bottom_i = old_rows - 1 if round(old_i) == old_rows - 1 else round(old_i)
-#             left_j = 0 if round(old_j) == 0 else round(old_j) - 1
-#             right_j = old_cols if round(old_j) == old_cols - 1 else round(old_j)
-#             width = math.fabs(0.5 - fract(old_i))
-#             height = math.fabs(0.5 - fract(old_j))
-#
-#             intensity_top = (1 - width) * original_img[top_i][left_j] + width * original_img[top_i][right_j]
-#             intensity_bottom = (1 - width) * original_img[bottom_i][left_j] + width * original_img[bottom_i][right_j]
-#             new_img[new_i][new_j] = (1 - height) * intensity_bottom + height * intensity_top
+# TODO: Remove prints and tuple()s
+def u(d):
+    print(d)
+    if math.fabs(d) < 1:
+        return 1.5 * (d ** 3) - 2.5 * (d ** 2) + 1
+    elif math.fabs(d) < 2:
+        return -0.5 * (d ** 3) + 2.5 * (d ** 2) - 4 * d + 2
+    return 0
+
+
+def get_cubic_matrix(start_i, start_j, d_i, d_j):
+    return tuple((tuple((u(math.fabs(i + d_i)) * u(math.fabs(j + d_j)) for j in range(start_j, start_j + 4)))
+                  for i in range(start_i, start_i + 4)))
+
+
+print("tl")
+cubic_tl_matrix = get_cubic_matrix(-2, -2, 0.25, 0.25)
+print("tr")
+cubic_tr_matrix = get_cubic_matrix(-2, -1, 0.25, -0.25)
+print("bl")
+cubic_bl_matrix = get_cubic_matrix(-1, -2, -0.25, 0.25)
+print("br")
+cubic_br_matrix = get_cubic_matrix(-1, -1, -0.25, -0.25)
+
+def get_matrix(roi):
+    if roi == (-2, -2):
+        return cubic_tl_matrix
+    if roi == (-2, -1):
+        return cubic_tr_matrix
+    if roi == (-1, -2):
+        return cubic_bl_matrix
+    if roi == (-1, -1):
+        return cubic_br_matrix
+
+def get_roi_range(fract_i, fract_j):
+    if fract_i < 0.5 and fract_j < 0.5:
+        return -2, -2  # tl
+    elif fract_i < 0.5 and fract_j >= 0.5:
+        return -2, -1  # tr
+    elif fract_i >= 0.5 and fract_j < 0.5:
+        return -1, -2  # bl
+    else:
+        return -1, -1  # br
+
+def interpolation_cubic(new_img, original_img, inverse_transformation):
+    new_rows, new_cols = new_img.shape
+    old_rows, old_cols = original_img.shape
+
+    # TODO: pad and shift indexes
+
+    for new_i in range(new_rows):
+        for new_j in range(new_cols):
+            old_i, old_j = calc_coordinates(inverse_transformation, new_i, new_j)
+            roi = get_roi_range(fract(old_i), fract(old_j))
+            matrix = get_matrix(roi)
+            start_i, start_j = roi
+
+            try:
+                matrix_roi = original_img[start_i: start_i + 4, start_j: start_j + 4]
+                new_value = np.float32(matrix_roi).inner(matrix)
+                new_img[new_i][new_j] = new_value
+            except:  # TODO: Remove
+                pass
