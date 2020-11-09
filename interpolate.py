4af6baafe6148d42bedd72ec26d30272a076382e
sagivbgu <sagivsc@post.bgu.ac.il>
2020-11-09 18:02:34 +0200

    Added interpolate.py documentation

--- interpolate.py
+++ interpolate.py
@@ -17,8 +17,10 @@ def fract(num):
     return num - math.floor(num)
 
 
-# Main interpolation loop, calling the desired interpolation function in each iteration
 def interpolate(new_img, original_img, inverse_transformation, interpolation_func):
+    """
+    Main interpolation loop, calling the desired interpolation function in each iteration
+    """
     new_rows, new_cols = new_img.shape
     old_rows, old_cols = original_img.shape
 
@@ -39,14 +41,18 @@ def interpolate(new_img, original_img, inverse_transformation, interpolation_fun
             new_img[new_y][new_x] = new_value
 
 
-# Nearest neighbor interpolation
-# Function signature follows interpolate function signature, though in this case the last arguments are not used
 def interpolate_nearest(original_img, old_x, old_y, old_rows, old_cols):
+    """
+    Nearest neighbor interpolation
+    Function signature follows interpolate function signature, though in this case the last arguments are not used
+    """
     return original_img[round(old_y)][round(old_x)]
 
 
-# Bilinear interpolation
 def interpolate_bilinear(original_img, old_x, old_y, old_rows, old_cols):
+    """
+    Bilinear interpolation
+    """
     # Get boundaries, also handling the edges of the image
     top_y = 0 if round(old_y) == 0 else round(old_y) - 1
     bottom_y = top_y if top_y == old_rows - 1 else top_y + 1
@@ -61,9 +67,11 @@ def interpolate_bilinear(original_img, old_x, old_y, old_rows, old_cols):
     return (1 - height) * intensity_bottom + height * intensity_top
 
 
-# Bicubic interpolation
-# Function signature follows interpolate function signature, though in this case the last arguments are not used
 def interpolate_cubic(original_img, old_x, old_y, old_rows, old_cols):
+    """
+    Bicubic interpolation
+    Function signature follows interpolate function signature, though in this case the last arguments are not used
+    """
     roi = get_roi(fract(old_x), fract(old_y))
     weight_matrix = get_weight_matrix(roi)
     start_x, start_y = roi
@@ -73,8 +81,10 @@ def interpolate_cubic(original_img, old_x, old_y, old_rows, old_cols):
     return calculate_cubic_new_value(matrix_roi, weight_matrix)
 
 
-# Get locations relative to the examined pixel to start the cubic interpolation matrix from
 def get_roi(fract_x, fract_y):
+    """
+    Get locations relative to the examined pixel to start the cubic interpolation matrix from
+    """
     if fract_y < 0.5 and fract_x < 0.5:
         return TL_ROI
     elif fract_y < 0.5 and fract_x >= 0.5:
@@ -85,8 +95,12 @@ def get_roi(fract_x, fract_y):
         return BR_ROI
 
 
-# The weight function in the interpolation formula. d is the distance
+
 def u(d):
+    """
+    The weight function in the interpolation formula
+    :param d: The distance (according to the formula)
+    """
     if math.fabs(d) < 1:
         return 1.5 * (d ** 3) - 2.5 * (d ** 2) + 1
     elif math.fabs(d) < 2:
@@ -95,10 +109,19 @@ def u(d):
 
 
 def get_cubic_matrix(start_x, start_y, d_x, d_y):
+    """
+    Calculate the interpolation matrix
+    :param start_x: The relative pixel to start the matrix from (x axis)
+    :param start_y: The relative pixel to start the matrix from (x axis)
+    :param d_x: The distance to add to the calculation (x axis)
+    :param d_y: The distance to add to the calculation (y axis)
+    :return:
+    """
     return tuple((tuple((u(math.fabs(x + d_x)) * u(math.fabs(y + d_y)) for x in range(start_x, start_x + 4)))
                   for y in range(start_y, start_y + 4)))
 
 
+# Weight matrices corresponding the examined position in pixel
 cubic_tl_matrix = get_cubic_matrix(*TL_ROI, 0.25, 0.25)
 cubic_tr_matrix = get_cubic_matrix(*TR_ROI, -0.25, 0.25)
 cubic_bl_matrix = get_cubic_matrix(*BL_ROI, 0.25, -0.25)
@@ -106,6 +129,9 @@ cubic_br_matrix = get_cubic_matrix(*BR_ROI, -0.25, -0.25)
 
 
 def get_weight_matrix(roi):
+    """
+    Get the weight matrix corresponding the examined position in pixel
+    """
     if roi == TL_ROI:
         return cubic_tl_matrix
     if roi == TR_ROI:
@@ -119,6 +145,11 @@ def get_weight_matrix(roi):
 
 
 def calculate_cubic_new_value(mat_roi, mat):
+    """
+    Calculate the new pixel value, applying the interpolation transformation on the original image
+    :param mat_roi: The original matrix pixels in the relevant area range of interest
+    :param mat: The interpolation matrix
+    """
     mat_roi = np.float32(mat_roi).flatten()
     mat = np.float32(mat).flatten()
     return np.inner(mat_roi, mat)
